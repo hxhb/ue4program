d51dafe6fc596aa86447ca479d0d15ffa76b50cf
hxhb <hxhb@live.com>
2019-08-20 10:24:34 +0800

    fix word error

--- ProgramTemplateText/PullBinaries_Bat.h
+++ ProgramTemplateText/PullBinaries_Bat.h
@@ -6,20 +6,19 @@
 
 const char* PROGRAM_TEMPLATE_PULL_BINARIES_BAT_PATH=R"(\\)";
 const char* PROGRAM_TEMPLATE_PULL_BINARIES_BAT_FILE_NAME=R"(.bat)";
-const char* PROGRAM_TEMPLATE_PULL_BINARIES_BAT=R"(
-@echo off
-set PROJECT_NAME=ProgramTemplateTarget
-md %%PROJECT_NAME%%\\Engine\\Binaries\\Win64
-md %%PROJECT_NAME%%\\Engine\\Content\\Internationalization\\icudt53l
-md %%PROJECT_NAME%%\\Engine\\Content\\Slate 
-md %%PROJECT_NAME%%\\Engine\\Shaders\\StandaloneRenderer
-copy ..\\..\\..\\Binaries\\Win64\\%%PROJECT_NAME%%.exe %%PROJECT_NAME%%\\Engine\\Binaries\\Win64
-copy Register.reg %%PROJECT_NAME%%\\
-xcopy /y/i/s/e ..\\..\\..\\Content\\Internationalization\\English\\icudt53l %%PROJECT_NAME%%\\Engine\\Content\\Internationalization\\icudt53l
-xcopy /y/i/s/e ..\\..\\..\\Content\\Slate %%PROJECT_NAME%%\\Engine\\Content\\Slate
-xcopy /y/i/s/e ..\\..\\..\\Shaders\\StandaloneRenderer %%PROJECT_NAME%%\\Engine\\Shaders\\StandaloneRenderer
-copy Resources\\icon.ico %%PROJECT_NAME%%\\Engine\\Content\\Slate\\Icons
-echo "Engine\\Binaries\\Win64\\%%PROJECT_NAME%%.exe">%%PROJECT_NAME%%\\%%PROJECT_NAME%%.bat
+const char* PROGRAM_TEMPLATE_PULL_BINARIES_BAT=R"(@echo off
+set PROJECT_NAME=ProgramTemplate
+md %PROJECT_NAME%\\Engine\\Binaries\\Win64
+md %PROJECT_NAME%\\Engine\\Content\\Internationalization\\icudt53l
+md %PROJECT_NAME%\\Engine\\Content\\Slate 
+md %PROJECT_NAME%\\Engine\\Shaders\\StandaloneRenderer
+copy ..\\..\\..\\Binaries\\Win64\\%PROJECT_NAME%.exe %PROJECT_NAME%\\Engine\\Binaries\\Win64
+copy Register.reg %PROJECT_NAME%\\
+xcopy /y/i/s/e ..\\..\\..\\Content\\Internationalization\\English\\icudt53l %PROJECT_NAME%\\Engine\\Content\\Internationalization\\icudt53l
+xcopy /y/i/s/e ..\\..\\..\\Content\\Slate %PROJECT_NAME%\\Engine\\Content\\Slate
+xcopy /y/i/s/e ..\\..\\..\\Shaders\\StandaloneRenderer %PROJECT_NAME%\\Engine\\Shaders\\StandaloneRenderer
+copy Resources\\icon.ico %PROJECT_NAME%\\Engine\\Content\\Slate\\Icons
+echo "Engine\\Binaries\\Win64\\%PROJECT_NAME%.exe">%PROJECT_NAME%\\%PROJECT_NAME%.bat
 )";
 
 
